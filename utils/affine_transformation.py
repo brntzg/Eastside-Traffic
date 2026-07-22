69f1b124197104bf7e0197419514d42232371776
brntzg <stephen.brantzeg@icloud.com>
2026-07-22 23:50:25 +0100

    Updated the Affine transform function name to be generic from read_pgwx to read_world_file; added a description and more explicit comments to the .py file; replaced the new function in .ipynb

--- utils/affine_transformation.py
+++ utils/affine_transformation.py
@@ -1,19 +1,32 @@
 from pathlib import Path
 from affine import Affine
 
-def read_pgwx(pgwx_path: Path) -> Affine:
-    values = [float(value) for value in pgwx_path.read_text().split()]
+def read_world_file(world_file_path: Path | str) -> Affine:
+    """
+    Read a world file as an Affine transform for file extensions: .pgw, .pgwx, .jgw, .jgwx, .tfw, .tfwx, .wld.
+    """
 
+    path = Path(world_file_path)
+
+    values = [float(value) for value in path.read_text().split()]
+
+    # Raise a Value Error if the file doe not contain six values:
     if len(values) != 6:
         raise ValueError(
-            f"{pgwx_path.name} contains {len(values)} values; expected 6."
+            f"{path.name} contains {len(values)} values; 6 expected."
         )
 
-    # World-file order
+    # World-file order:
+        # A: pixel width (x-scale), 
+        # D: row rotation (y-skew),
+        # B: column rotation (x-skew),
+        # E: pixel height (y-scale),
+        # C: upper-left pixel-center X,
+        # F: upper-left pixel-center Y
     A, D, B, E, C, F = values
 
-    # World files reference the centre of the upper-left pixel.
-    # Rasterio uses the outer upper-left corner.
+    # World files reference the center of the upper-left pixel.
+    # Rasterio/Affine transforms uses the outer upper-left corner.
     return Affine(
         A,
         B,
